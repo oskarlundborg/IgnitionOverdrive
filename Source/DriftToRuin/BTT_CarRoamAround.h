fbf745a0a2539a918f8b56c8f92fbf0dbf2bf746
Daniel Olsson <125807379+OfficialBabyboidaniel@users.noreply.github.com>
2023-11-06 16:28:26 +0100

    start of car roam around BBT task
    - gjorde ett task script som en början av AI rörelse beteende.
    - antagligen kommer den brytas upp till två separata tasks i framtiden.

--- Source/DriftToRuin/BTT_CarRoamAround.h
+++ Source/DriftToRuin/BTT_CarRoamAround.h
@@ -13,5 +13,24 @@ UCLASS()
 class DRIFTTORUIN_API UBTT_CarRoamAround : public UBTTaskNode
 {
 	GENERATED_BODY()
-	
+
+public:
+	UPROPERTY(EditAnywhere, Category = "Car Parameters")
+	TArray<int32> MeterRange;
+
+	UPROPERTY(EditAnywhere, Category = "Car Parameters")
+	int32 LowestMeterDistance;
+
+	UPROPERTY(EditAnywhere, Category = "Car Parameters")
+	int32 HighestMeterDistance;
+
+	UPROPERTY(EditAnywhere, Category = "Car Parameters")
+	int32 InitialDegreeViewAtStartGame; //maybe this can be the camera view ?  
+
+	EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory);
+
+private:
+	FVector PointLocation;
+
+	bool MoveToPoint(FVector& Point, class APawn* Pawn);
 };
