90c5512a520f7a12009a0b2681540f12f45548ca
Gin Lindelöw <lindelowgin@gmail.com>
2023-11-08 12:34:46 +0100

    Basic setup.

---
+++ Source/DriftToRuin/DeformTest.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DeformTest.h"
+
+// Sets default values
+ADeformTest::ADeformTest()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ADeformTest::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADeformTest::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
