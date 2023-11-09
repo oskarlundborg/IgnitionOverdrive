c15525881daa2b3a122d6a35f0050be9d1063884
Joakim Pettersson <joakimbjorn.pettersson@gmail.com>
2023-11-09 16:06:14 +0100

    boat done

--- Source/DriftToRuin/BaseVehiclePawn.cpp
+++ Source/DriftToRuin/BaseVehiclePawn.cpp
@@ -3,13 +3,39 @@
 
 #include "BaseVehiclePawn.h"
 #include "HealthComponent.h"
+#include "ChaosWheeledVehicleMovementComponent.h"
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 
 ABaseVehiclePawn::ABaseVehiclePawn()
 {
     //Get Vehicle Movement Component when its needed
+	UChaosWheeledVehicleMovementComponent* VehicleMovementComp = CastChecked<UChaosWheeledVehicleMovementComponent>(GetVehicleMovement());
+
+	//Engine value defaults
+	VehicleMovementComp->EngineSetup.MaxTorque = 1200.f;
+	VehicleMovementComp->EngineSetup.EngineIdleRPM = 1500.f;
+	VehicleMovementComp->EngineSetup.TorqueCurve.GetRichCurve()->Reset();
+	VehicleMovementComp->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(0.0f, 450.0f);
+	VehicleMovementComp->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(1000.0f, 750.0f);
+	VehicleMovementComp->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(2000.0f, 1000.0f);
+	VehicleMovementComp->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(5500.0f, 1193.0f);
+	VehicleMovementComp->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(8000.0f, 1000.0f);
 	
+	
+	//Steering value defaults
+	VehicleMovementComp->SteeringSetup.SteeringCurve.GetRichCurve()->Reset();
+	VehicleMovementComp->SteeringSetup.SteeringCurve.GetRichCurve()->AddKey(0.0f, 1.0f);
+	VehicleMovementComp->SteeringSetup.SteeringCurve.GetRichCurve()->AddKey(40.0f, 0.7f);
+	VehicleMovementComp->SteeringSetup.SteeringCurve.GetRichCurve()->AddKey(120.0f, 0.6f);
+
+	//Differential value defaults
+	VehicleMovementComp->DifferentialSetup.DifferentialType = EVehicleDifferential::AllWheelDrive;
+	VehicleMovementComp->DifferentialSetup.FrontRearSplit = 0.7f;
+
+	//Gearbox value defaults
+	VehicleMovementComp->TransmissionSetup.bUseAutomaticGears = true;
+	VehicleMovementComp->TransmissionSetup.GearChangeTime = 0.25f;
 
 	//Creates Health Component and sets it max health value
 	HealthComponent = CreateDefaultSubobject<UHealthComponent>(TEXT("HealthComponent"));
