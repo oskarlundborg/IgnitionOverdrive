9708b05267c9446b1fd7f979b0ed703449c7f123
Joakim Pettersson <joakimbjorn.pettersson@gmail.com>
2023-11-14 12:29:18 +0100

    Engine revert
    gamla values till torque då det visst var dom jag testat med hela tiden

--- Source/DriftToRuin/BaseVehiclePawn.cpp
+++ Source/DriftToRuin/BaseVehiclePawn.cpp
@@ -17,11 +17,11 @@ ABaseVehiclePawn::ABaseVehiclePawn()
 	VehicleMovementComp->EngineSetup.MaxTorque = 1200.f;
 	VehicleMovementComp->EngineSetup.EngineIdleRPM = 1500.f;
 	VehicleMovementComp->EngineSetup.TorqueCurve.GetRichCurve()->Reset();
-	VehicleMovementComp->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(0.0f, 500.0f);
-	VehicleMovementComp->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(1000.0f, 600.0f);
-	VehicleMovementComp->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(2000.0f, 800.0f);
-	VehicleMovementComp->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(5500.0f, 853.0f);
-	VehicleMovementComp->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(8000.0f, 800.0f);
+	VehicleMovementComp->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(0.0f, 800.0f);
+	VehicleMovementComp->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(1000.0f, 900.0f);
+	VehicleMovementComp->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(2000.0f, 1000.0f);
+	VehicleMovementComp->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(5500.0f, 1193.0f);
+	VehicleMovementComp->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(8000.0f, 1000.0f);
 	
 	
 	//Steering value defaults
