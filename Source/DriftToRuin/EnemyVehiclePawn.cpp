ce49a6b19ec7a82461858cb7d1698359890a14fb
Daniel Olsson <125807379+OfficialBabyboidaniel@users.noreply.github.com>
2023-11-14 13:29:20 +0100

    AI
    Added tag "player" for ai and player BP
    added code to c++ AI base class. turret, minigun, and homing launcher set up.
    Created BP for AI cars

---
+++ Source/DriftToRuin/EnemyVehiclePawn.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyVehiclePawn.h"
+#include "HomingMissileLauncher.h"
+#include "Minigun.h"
+#include "PlayerTurret.h"
+
+void AEnemyVehiclePawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	Turret = GetWorld()->SpawnActor<APlayerTurret>(PlayerTurretClass);
+	Turret->AttachToComponent(GetMesh(), FAttachmentTransformRules::KeepRelativeTransform, TEXT("TurretSocket"));
+	Turret->SetOwner(this);
+
+	Minigun = GetWorld()->SpawnActor<AMinigun>(MinigunClass);
+	Minigun->AttachToComponent(Turret->GetTurretMesh(), FAttachmentTransformRules::SnapToTargetNotIncludingScale, TEXT("MinigunSocket"));
+	Minigun->SetOwner(this);
+
+	HomingLauncher = GetWorld()->SpawnActor<AHomingMissileLauncher>(HomingLauncherClass);
+	HomingLauncher->AttachToComponent(Turret->GetTurretMesh(), FAttachmentTransformRules::SnapToTargetNotIncludingScale, TEXT("HomingSocket"));
+	HomingLauncher->SetOwner(this);
+}
