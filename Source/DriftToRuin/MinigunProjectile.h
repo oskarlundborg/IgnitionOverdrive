c0a055adb0c493a43c145bd5b4e1aab01de349a5
mihajlor <mihajloradotic2@gmail.com>
2023-11-21 00:04:46 +0100

    Fixed Vehicles fly away bug

--- Source/DriftToRuin/MinigunProjectile.h
+++ Source/DriftToRuin/MinigunProjectile.h
@@ -19,6 +19,7 @@ public:
 
 private:
 	virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpusle, const FHitResult& Hit) override;
+	virtual void OnOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
 
 protected:
 	virtual void BeginPlay() override;
