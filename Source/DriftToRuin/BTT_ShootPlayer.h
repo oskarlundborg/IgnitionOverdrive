40790496f591b04c1d4e29c009e5fb4ee77d358f
Daniel Olsson <125807379+OfficialBabyboidaniel@users.noreply.github.com>
2023-11-29 21:27:14 +0100

    homing missile attack

--- Source/DriftToRuin/BTT_ShootPlayer.h
+++ Source/DriftToRuin/BTT_ShootPlayer.h
@@ -6,6 +6,7 @@
 #include "BehaviorTree/BTTaskNode.h"
 #include "BTT_ShootPlayer.generated.h"
 
+class AHomingMissileLauncher;
 class AMinigun;
 class APlayerTurret;
 /**
@@ -15,12 +16,15 @@ UCLASS()
 class DRIFTTORUIN_API UBTT_ShootPlayer : public UBTTaskNode
 {
 	GENERATED_BODY()
+
 public:
 	UBTT_ShootPlayer();
+	bool InitializeEverything(UBehaviorTreeComponent& OwnerComp, EBTNodeResult::Type& Value1);
 	EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory);
 	virtual void TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
 
 	bool HasKilled = false;
+
 private:
 	bool InitializeAIComponents(UBehaviorTreeComponent& OwnerComp);
 	APawn* AIPawn;
@@ -28,6 +32,7 @@ private:
 	UBlackboardComponent* BlackboardComp;
 	AMinigun* Minigun = nullptr;
 	APlayerTurret* PlayerTurret = nullptr;
+	AHomingMissileLauncher* HomingMissileLauncher = nullptr;
 
 	//enemy
 	FVector EnemyLocation;
@@ -36,4 +41,16 @@ private:
 	FRotator NewRotation;
 	FRotator TargetRotation;
 	float InterpSpeed = 1;
+
+	bool Overheating = false;
+	bool MinigunPulledTrigger = false;
+	bool MissilePulledTrigger = false;
+	bool MissileIsAvailable = false;
+
+	bool InitializedComponents = false;
+
+	int32 MissileChargeAmount = FMath::RandRange(1, 3);
+
+	//result type
+	EBTNodeResult::Type TaskResult;
 };
