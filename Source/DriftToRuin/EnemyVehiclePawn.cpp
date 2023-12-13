0fb1b5112baa6eed1a1533b74b983d1325c61392
Daniel Olsson <125807379+OfficialBabyboidaniel@users.noreply.github.com>
2023-12-13 10:24:50 +0100

    fix så ai glömmer bort fienden som sköt efter 3 sekunder. nytt beteende, reagera på när man blir skjuten

--- Source/DriftToRuin/EnemyVehiclePawn.cpp
+++ Source/DriftToRuin/EnemyVehiclePawn.cpp
@@ -85,8 +85,9 @@ void AEnemyVehiclePawn::Tick(float DeltaSeconds)
 	{
 		DrivePath();
 	}
-	else if (SwitchString == "Rush")
+	else if (SwitchString == "ReactToGetShot")
 	{
+		ReactToGetShot();
 		// Code for Case3
 	}
 	else
@@ -118,6 +119,17 @@ void AEnemyVehiclePawn::DriveAndShoot()
 	ManageSpeed();
 
 	CheckIfAtEndOfSpline();
+}
+
+void AEnemyVehiclePawn::ReactToGetShot()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Reacting to get shot"));
+
+	RotateTowardsShootingEnemy();
+	DriveAlongSpline();
+	ManageSpeed();
+	CheckIfAtEndOfSpline();
+	
 }
 
 void AEnemyVehiclePawn::Shoot()
@@ -265,6 +277,27 @@ void AEnemyVehiclePawn::AddNewTurretRotation()
 	TargetRotation.Yaw = CarRotation.Yaw - TurretRotation.Yaw + RotationIncrement.Yaw;
 
 	TimerIsActive = false;
+}
+
+void AEnemyVehiclePawn::RotateTowardsShootingEnemy()
+{
+	UObject* ShootingEnemy = BlackboardComp->GetValueAsObject("GotShotByEnemy");
+	AActor* ShootingEnemyActor = Cast<AActor>(ShootingEnemy);
+	if(ShootingEnemyActor == nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("shootingenemyactor nullptr"));
+		return;
+	}
+	
+	TargetRotation = UKismetMathLibrary::FindLookAtRotation(GetActorLocation(), ShootingEnemyActor->GetActorLocation());
+
+	NewRotation = FMath::RInterpTo(Turret->GetActorRotation(), TargetRotation,
+								   GetWorld()->GetDeltaSeconds(), RotationInterpSpeed);
+	if (Turret != nullptr)
+	{
+		Turret->SetActorRotation(NewRotation);
+	}
+
 }
 
 void AEnemyVehiclePawn::ManageSpeed()
