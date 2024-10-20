b61a7f6b48512e253440e2101e098145d19a3a9e
Daniel <danielspider2002@gmail.com>
2024-10-20 21:57:00 +0200

    first commit

--- Source/DriftToRuin/IgnitionOverdriveGameInstance.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "IgnitionOverdriveGameInstance.h"
-
-TArray<AActor *> UIgnitionOverdriveGameInstance::GetPlayerStarts()
-{
-    return PlayerStarts;
-}
